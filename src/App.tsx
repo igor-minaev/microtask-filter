bbfdc4fec20d72278931afb1c8589c5091ff1de6
Igor Minayeu <igor.minayeu@gmail.com>
2023-09-27 21:08:18 +0300

    start microtask-filter

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+    const [money, setMoney] = useState([
+        { banknots: 'Dollars', value: 100, number: ' a1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' z1234567890' },
+        { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
+        { banknots: 'Dollars', value: 100, number: ' e1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' c1234567890' },
+        { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' x1234567890' },
+        { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+    ])
+
+    let currentMoney = money.filter(filteredMoney=>filteredMoney.banknots === 'RUBLS')
+    return (
+        <ul>
+            {currentMoney.map(m=>{
+                return (
+                    <li key={m.number}>
+                        <span>{m.banknots} </span>
+                        <span>{m.value} </span>
+                        <span>{m.number}</span>
+                    </li>
+                )
+            })}
+        </ul>
+    );
+}
+
+export default App;
